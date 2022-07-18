86481d7cb2faae48a8f03c554f792f155a36d846
Kristian Damian <kristian.damian@themxgroup.com>
2022-07-18 10:21:57 -0500

    add basic axios structure

---
+++ src/lib/axios-factory.tsx
@@ -0,0 +1,17 @@
+import axios, { AxiosInstance } from "axios";
+
+const API_KEY = process.env.QUIZ_KEY;
+
+export class AxiosFactory {
+	async create(): Promise<AxiosInstance> {
+		const headers: any = {};
+		headers["X-Api-Key"] = API_KEY;
+
+		const instance = axios.create({
+			headers,
+		});
+		return instance;
+	}
+}
+
+export const axiosFactory = new AxiosFactory();
--- src/services/questions.tsx
+++ src/services/questions.tsx
@@ -1,6 +1,11 @@
-import axios from "axios";
+import { axiosFactory } from "../lib/axios-factory";
 
+
+const baseUri =  "https://quizapi.io/api/v1";
 
 export async function getQuestions() {
-    axios.get("")
+    const axios = await axiosFactory.create();
+    const response = await axios.get(`${baseUri}/api/membership/password/forgot`, payload);
+
+    return response.data;
 }
